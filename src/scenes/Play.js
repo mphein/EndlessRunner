ad54705df28facd59f2481fdeeec3663ee7ad0c2
Michael <mphein@ucsc.edu>
2023-05-05 13:09:47 -0700

    Finished stingray spritesheet

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,18 @@ class Play extends Phaser.Scene {
   
   preload() {
     this.load.image('ocean', './assets/endlessRunnerBackground.png');
-    this.load.image('stingray', './assets/stingray.png')
+    this.load.spritesheet('stingray', './assets/Stingray.png', {frameWidth: 96, frameHeight: 32, startFrame: 0, endFrame: 17});
   }
 
   create() {
     this.ocean = this.add.tileSprite(0,0,640,480,'ocean').setOrigin(0,0);
-    this.stingray = new Stingray(this, 50, game.config.height / 1.5, 'stingray').setOrigin(.5,0);
+    this.stingray = new Stingray(this, 50, game.config.height / 1.5, 'Stingray').setOrigin(.5,0);
+    this.anims.create({
+      key: 'swim',
+      frames: this.anims.generateFrameNumbers('stingray', {start: 0, end: 29, first: 0}),
+      frameRate: 17
+    })
+    this.stingray.play('swim');
   }
 
   update(){
